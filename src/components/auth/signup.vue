00b2c63b4b45592e957685f34e24d0de433af589
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2019-04-04 12:32:31 +0200

    fixed emeail signup button

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -31,7 +31,7 @@
         />
       </div>
       <div class="submit">
-        <button type="submit">Submit</button>
+        <button type="submit" :disabled="$v.$invalid">Submit</button>
       </div>
     </form>
   </div>
